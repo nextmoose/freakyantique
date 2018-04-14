6f6572572519f1b81d3c3ceeecec23a590e53436
Emory Merryman <emory.merryman@gmail.com>
2018-04-14 23:42:13 +0000

    some progress

--- myscript.js
+++ myscript.js
@@ -1,2 +1,9 @@
-document.getElementById('txthearingdate').parentNode.prepend(document.createElement('Yesterday').addEventListener(() => document.getElementById('txthearingdate').value='yesterday'));
+
+(button => [
+    (button.innerHTML='Yesterday'),
+    document.getElementById('txthearingdate').parentNode.prepend(button),
+    button.addEventListener('click', () => document.getElementById('txthearingdate').value='yesterday')
+])(document.createElement('button'))
+
+console.log('rock on');
 const fixit = (date) => document.getElementById('txthearingdate').value=date

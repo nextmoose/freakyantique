e070d85f44cd21f0a1010bc98990cfa79ece01b9
Emory Merryman <emory.merryman@gmail.com>
2018-04-14 22:34:32 +0000

    2

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => [
     document.getElementById('input').addEventListener('click', () => alert('input')),
-    document.getElementById('output').addEventListener('click', () => document.getElementById('result').value='hello')
+    document.getElementById('output').addEventListener('click', () => console.log('hello'))
 ])

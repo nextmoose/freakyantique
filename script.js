f485eed2c50811a42d807f29583d646b2648824f
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 19:38:01 +0000

    try to verify

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const querySelector = selector => new Promise((resolve, reject) => (element => e
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
 const setData = (key, value) => new Promise((resolve, reject) => chrome.storage.local.set({key: value}, () => resolve(value)));
 const getData = key => new Promise((resolve, reject) => chrome.storage.local.get(key, value => resolve(value)));
+
+alert('hi');
+
 querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
     .then(rows => rows
         .filter((element, index) => index > 0)
@@ -11,6 +14,6 @@ querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > tab
         }))
     ).then(objects => setData("objects", objects))
     .then(objects => console.log(JSON.stringify(objects)))
-    .then(expected => getData("objects").then(observed => console.log(expected === observed)))
+    .then(expected => getData("objects").then(observed => console.log(observed)))
 // querySelector("input[value='Next']")
 //     .then(element => element.click(), failure => console.log("The end of the road."))

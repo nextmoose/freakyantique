cbcc970d23a8940d8aef73ec2f99d96d0d2e6a10
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 19:49:33 +0000

    try to aggregate the data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
-const setData = value => new Promise((resolve, reject) => chrome.storage.local.set({key: value}, () => resolve(value)));
+const setData = value => new Promise((resolve, reject) => getData().then(alpha => (beta => chrome.storage.local.set({key: beta}, () => resolve(beta)))(alpha ? alpha : [])));
 const getData = () => new Promise((resolve, reject) => chrome.storage.local.get(["key"], value => resolve(value)));
 
 querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")

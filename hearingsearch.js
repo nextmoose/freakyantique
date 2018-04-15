c1bb0677e9e60fb39bdd060902045a2e20288c61
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 15:46:42 +0000

    months start at zero

--- hearingsearch.js
+++ hearingsearch.js
@@ -1,7 +1,7 @@
 Promise.all([
         querySelector("#txthearingdate"),
         createElement("button"),
-        querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form")
+        querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td > input:nth-child(1)")
     ])
     .then(success, failure)
     .then(obj => new Promise((resolve, reject) => {
@@ -10,7 +10,7 @@ Promise.all([
             obj[1].addEventListener('click', event => {
                 event.preventDefault();
                 obj[0].value = new Date(obj[0].value).yesterday().format();
-                obj[2].submit();
+                obj[2].click();
             })
             obj[0].value = "05/17/2018";
             obj[0].parentNode.prepend(obj[1]);

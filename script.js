2e2989cc1c2ddccf443ebee3e0f857a5ff85d948
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 21:05:37 +0000

    it looks good

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
 const requestFileSystem = () => new Promise((resolve, reject) => window.requestFileSystem(window.TEMPORARY, 5*1024*1024, resolve, reject));
-const getFile = (fileSystem, name, create) => new Promise((resolve, reject) => fileSystem.getFile(name, {create}, resolve, reject));
+const getFile = (fileSystem, name, create) => new Promise((resolve, reject) => fileSystem.root.getFile(name, {create}, resolve, reject));
 const createWriter = file => new Promise((resolve, reject) => file.createWrite(), resolve, reject);
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
 
-requestFileSystem().then(fileSystem => fileSystem.root.getFile("data.txt", true)).then(entry => entry.createWriter()).then(writer =>
+requestFileSystem().then(fileSystem => getFile(fileSystem, "data.txt", true)).then(entry => entry.createWriter()).then(writer =>
     querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
         .then(rows => rows
             .filter((element, index) => index > 0)

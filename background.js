6e1b4470f58cc69fa57a5a3ad5f32d19e91fd327
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 23:20:25 +0000

    fixed

--- background.js
+++ background.js
@@ -1,2 +1,2 @@
 const cases = [];
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => cases=cases.concat(request));
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => cases.push(request));

62fcbeb2d325a1d973968bbe4e98d9f37e904f11
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 19:51:06 +0000

    try to aggregate the data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
-const setData = value => new Promise((resolve, reject) => getData().then(alpha => (beta => chrome.storage.local.set({key: beta}, () => resolve(beta)))(alpha ? alpha : [])));
+const setData = value => new Promise((resolve, reject) => getData().then(alpha => (beta => chrome.storage.local.set({ key: beta }, () => resolve(beta)))(alpha ? alpha : [])));
 const getData = () => new Promise((resolve, reject) => chrome.storage.local.get(["key"], value => resolve(value)));
 
 querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
@@ -12,6 +12,6 @@ querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > tab
         }))
     ).then(objects => setData(objects))
     .then(objects => console.log(JSON.stringify(objects)))
-    .then(expected => getData().then(observed => console.log(observed)))
+    .then(expected => getData().then(observed => console.log(observed)), failure => console.log("FAILURE:  "+JSON.stringify(failure)))
 // querySelector("input[value='Next']")
 //     .then(element => element.click(), failure => console.log("The end of the road."))

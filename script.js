7b93a34aecd875fc4d8c5c020f6f35fcf27f107c
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 20:11:15 +0000

    maybe

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const querySelector = selector => new Promise((resolve, reject) => (element => e
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
 const setData = value => new Promise((resolve, reject) => chrome.storage.local.set({key: value}, () => resolve(value)));
 const getData = () => new Promise((resolve, reject) => chrome.storage.local.get(["key"], value => resolve(value)));
-const addData = value => getData().then(alpha => {
-   console.log(alpha);
-   console.log(value);
-   setData(value);
-});
+const addData = value => getData().then(alpha => setData((Array.isArray(alpha)?alpha:[]).concat(value)));
 querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
     .then(rows => rows
         .filter((element, index) => index > 0)

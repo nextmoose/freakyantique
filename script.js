4eecba91462208b01ddbc924382eadc0a68fb170
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 19:23:20 +0000

    try to store it

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
+const storeData = (key, value) => new Promise((resolve, reject) => chrome.storage.local.set({key: value}, () => resolve(value)));
 
 querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
     .then(rows => rows
@@ -8,6 +9,7 @@ querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > tab
             caseNumber: element.children[1].innerText,
             defendant: element.children[2].innerText
         }))
-    ).then(objects => console.log(JSON.stringify(objects)));
+    ).then(objects => storeData("objects", objects))
+    .then(objects => console.log(JSON.stringify(objects)));
 // querySelector("input[value='Next']")
 //     .then(element => element.click(), failure => console.log("The end of the road."))

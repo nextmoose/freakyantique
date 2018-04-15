1c20ea87df1e5713bd022b08ef7a3aee8d83267d
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 13:45:14 +0000

    try this

--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 querySelector("#txtcourts1")
     .then(element => element.value="Arlington General District Court")
-    .then(querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a")
+    .then(querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a"))
     .then(element => element.click())
     .then(success, failure);

6d49e7aad1b9d08b348d9054c3c2c468906ebc11
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 13:53:29 +0000

    try to be more careful

--- functions.js
+++ functions.js
@@ -2,14 +2,14 @@ const success = success => console.log("SUCCESS:  "+JSON.stringify(success));
 const failure = failure => console.log("FAILURE:  "+JSON.stringify(failure));
 const querySelector = selector => new Promise((resolve, reject) => {
     try {
-        resolve(document.querySelector(selector));
+        setTimeout(()=>resolve(document.querySelector(selector)),1000);
     }catch(cause){
         reject(cause);
     }
 });
-const createElement = () => new Promise((resolve, reject) => {
+const createElement = tag => new Promise((resolve, reject) => {
    try {
-       resolve(document.createElement());
+       resolve(document.createElement(tag));
    }catch(cause){
        reject(cause);
    }

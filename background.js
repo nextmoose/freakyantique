dc62ed8ee365a0ffcd5a675f4c155901fe74fa9f
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 23:16:09 +0000

    this might work

--- background.js
+++ background.js
@@ -1,1 +1,2 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => console.log({request, sender, sendResponse}));
+const cases = [];
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => cases=cases.concat(request));
--- scratch.js
+++
@@ -1,5 +0,0 @@
-requestFileSystem()
-    .then(fileSystem => getFile(fileSystem, "wtf.txt",true))
-    .then(file => createWriter(file))
-    .then(writer => writer.write(new Blob(["alpha"], {type: "application/json"}))
-    .then(success => console.log("SUCCESS"), failure => console.log("FAILURE"))

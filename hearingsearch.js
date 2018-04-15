906ff00a9e3b790ac4f6dd25c8cbde40bd9f97a2
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 13:55:00 +0000

    olog it

--- hearingsearch.js
+++ hearingsearch.js
@@ -2,3 +2,4 @@ Promise.all({
     hearingDateElement: querySelector("#txthearingdate"),
     button: createElement("button").then(element => element.innerHTML="Yesterday")
 }).then(obj => obj.hearingDateElement.parentNode.prepend(obj.button))
+.then(success, failure);

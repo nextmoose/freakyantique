dcc366797c28e7baa80854b508debbbf39f7b04b
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 19:04:51 +0000

    restart

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert("hello")
+const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
+querySelector("input[value='Next']")
+    .then(element => element.click(), failure => console.log("The end of the road."))
+    

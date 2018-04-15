ad5ff027a8a29da4e2e60121abe57856acfa171b
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 14:09:55 +0000

    use 10 seconds

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ const success = success => console.log("SUCCESS:  "+JSON.stringify(success));
 const failure = failure => console.log("FAILURE:  "+JSON.stringify(failure));
 const querySelector = selector => new Promise((resolve, reject) => {
     try {
-        setTimeout(()=>resolve(document.querySelector(selector)),1000);
+        setTimeout(()=>resolve(document.querySelector(selector)),10000);
     }catch(cause){
         reject(cause);
     }

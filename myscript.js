219ed432ff2e99f4b243faa80d686ddb2287d7bc
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 00:19:04 +0000

    tryit

--- myscript.js
+++ myscript.js
@@ -2,13 +2,13 @@
 
 Date.prototype.yesterday = function() {return new Date(this.getTime()-24*60*60*1000)}
 HTMLButtonElement.prototype.setText = function(value) {return (this.innerHTML=value)||true}
-HTMLInputElement.prototype.yesterday = function() {return this.value=this.value.yesterday()}
 
+console.log(new Date().yesterday());
 
 (button =>
     button.setText("Yesterday") &&
     document.getElementById('txthearingdate').parentNode.prepend(button) &&
-    button.addEventListener('click', event => console.log(Object.prototype.toString.apply(event)))
+    button.addEventListener('click', event => event.preventDefault())
 )(document.createElement('button'))
 
 console.log('rock on');

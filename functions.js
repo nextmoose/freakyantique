0cd90cb84ee63d8f060729202c93bc810491799b
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 14:24:37 +0000

    try  waiting

--- functions.js
+++ functions.js
@@ -1,9 +1,14 @@
 const success = success => console.log("SUCCESS:  "+JSON.stringify(success));
 const failure = failure => console.log("FAILURE:  "+JSON.stringify(failure));
 const querySelector = selector => new Promise((resolve, reject) => {
-    try{
-        resolve(document.querySelector(selector));
-    }catch(cause){
+    try {
+        const element = resolve(document.querySelector(selector));
+        if(element){
+            resolve(element);
+        }else{
+            setTimeout(querySelector(selector).then(success => resolve(success), failure => reject(failure)), 1000)
+        }
+    } catch(cause){
         reject(cause);
     }
 });

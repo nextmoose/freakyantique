9d1c033aef3bb3c337f3aa5e67a805ebebaaac25
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 00:41:28 +0000

    shutdown cleanup commit

--- myscript.js
+++ myscript.js
@@ -2,12 +2,11 @@
 
 Date.prototype.yesterday = function() {return new Date(this.getTime()-24*60*60*1000)}
 HTMLButtonElement.prototype.setText = function(value) {return (this.innerHTML=value)||true}
-
-console.log(new Date().yesterday());
+HTMLButtonElement.prototype.addEventListener2 = function(e,c) {return (this.addEventListener(e,c))||true}
 
 (button =>
     button.setText("Yesterday") &&
-    button.addEventListener('click', event => event.preventDefault()) &&
+    (button.addEventListener2('click', event => event.preventDefault()) || true) &&
     document.getElementById('txthearingdate').parentNode.prepend(button) &&
     console.log('did it')
 )(document.createElement('button'))

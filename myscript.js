4b7a2d854778d7011d6201b9d1db9eb4b7be4d69
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 00:11:12 +0000

    use prototypes

--- myscript.js
+++ myscript.js
@@ -1,9 +1,15 @@
 
-(button => [
-    (button.innerHTML='Yesterday') &&
-    document.getElementById('txthearingdate').parentNode.prepend(button),
-    button.addEventListener('click', event => [(event.preventDefault()), document.getElementById('txthearingdate').value='yesterday'])
-])(document.createElement('button'))
+
+Date.prototype.yesterday = function() {return new Date(this.getTime()-24*60*60*1000)}
+HTMLButtonElement.prototype.setText = function(value) {return (this.innerHTML=value)||true}
+HTMLInputElement.prototype.yesterday = function() {return this.value=this.value.yesterday()}
+
+
+(button =>
+    button.setText("Yesterday") &&
+    document.getElementById('txthearingdate').parentNode.prepend(button) &&
+    button.addEventListener('click', event => console.log(Object.prototype.toString.apply(event)))
+)(document.createElement('button'))
 
 console.log('rock on');
 const fixit = (date) => document.getElementById('txthearingdate').value=date

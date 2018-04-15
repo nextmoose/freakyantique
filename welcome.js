493342c92ed202ecec988a9ea0d5b4da3f66aecc
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 15:10:52 +0000

    try this

--- welcome.js
+++ welcome.js
@@ -2,4 +2,14 @@ Promise.all([
         querySelector("#txtcourts1"),
         querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a")
     ])
-    .then(success, failure);
+    .then(success, failure)
+    .then(obj => new Promise((resolve, reject) => {
+        try{
+            obj[0].value = "Arlington General District Court";
+            obj[1].click();
+            resolve();
+        }catch(cause){
+            reject(cause);
+        }
+    }))
+    .then(success, failure)

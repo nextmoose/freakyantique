51eadb68d3b747fa305a33212d9fbc4653b2c993
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 20:57:19 +0000

    ask for persistence

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-navigator.storage && navigator.storage.persist && navigator.storage.persist().then(granted => granted ? console.log("Persistence") : console.log("No Persistence"));
+
+const requestFileSystem = () => new Promise((resolve, reject) => (window.requestFileSystem || window.webkitRequestFileSystem).requestFileSystem(window.TEMPORARY, 5*1024*1024, resolve, reject));
+const getFile = (fileSystem, name, create) => new Promise((resolve, reject) => fileSystem.getFile(name, {create}, resolve, reject));
+const createWriter = file => new Promise((resolve, reject) => file.createWrite(), resolve, reject);
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
-const setData = value => new Promise((resolve, reject) => chrome.storage.local.set({key: value}, () => resolve(value)));
-const getData = () => new Promise((resolve, reject) => chrome.storage.local.get(["key"], value => resolve(value)));
-const addData = value => getData().then(alpha => {
-    console.log(Array.isArray(alpha)?alpha:[]);
-    const x = setData((Array.isArray(alpha)?alpha:[]).concat(value))
-    return x;
-});
-// const addData = value => getData().then(alpha => setData((Array.isArray(alpha)?alpha:[]).concat(value)));
-const sleep = time => new Promise((resolve, reject) => setTimeout(resolve, time));
-querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
-    .then(rows => rows
-        .filter((element, index) => index > 0)
-        .map(element => ({
-            caseNumber: element.children[1].innerText,
-            defendant: element.children[2].innerText
-        }))
-    ).then(objects => addData(objects))
-    .then(objects => console.log(objects))
-    .then(querySelector("input[value='Next']").then(element => element.click(), failure => console.log("The end of the road.")))
-    .then(expected => getData().then(observed => console.log(observed)), failure => console.log("FAILURE:  "+JSON.stringify(failure)))
+
+requestFileSystem().then(fileSystem => fileSystem.root.getFile("data.txt", true)).then(entry => entry.createWriter()).then(writer =>
+    querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
+        .then(rows => rows
+            .filter((element, index) => index > 0)
+            .map(element => ({
+                caseNumber: element.children[1].innerText,
+                defendant: element.children[2].innerText
+            }))
+        ).then(objects => writer.write)
+        .then(querySelector("input[value='Next']").then(element => element.click(), failure => console.log("The end of the road.")))
+);

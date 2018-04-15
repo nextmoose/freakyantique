2debb632f66417cf6224d4594d274550ff4d21f6
Emory Merryman <emory.merryman@gmail.com>
2018-04-15 14:11:26 +0000

    even 10 seconds is not enought

--- hearingsearch.js
+++ hearingsearch.js
@@ -2,4 +2,5 @@
 //         "gibberish",
 //         querySelector("#txthearingdate")
 //     ])
+console.log(querySelector("#txthearingdate"))
     querySelector("#txthearingdate").then(success, failure);

77161ddedea26565756806ccba6b37f8736d2693
Emory Merryman <emory.merryman@gmail.com>
2018-04-16 02:21:01 +0000

    filters

--- filter.js
+++ filter.js
@@ -1,1 +1,3 @@
 [].concat.apply([],cases).filter(element => element.complainant==="?")
+[].concat.apply([],cases).filter(element => element.charge==="DISRE HWY SIGN").filter(element => element.result!=="")
+[].concat.apply([],cases).map(element => element.result).filter((element, index, array) => array.indexOf(element)===index)

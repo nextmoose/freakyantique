5631e6fbb5420ee637d9b319437fc73af312a5f4
Emory Merryman <emory.merryman@gmail.com>
2018-04-16 12:44:37 +0000

    do it

--- casenumber.js
+++ casenumber.js
@@ -3,3 +3,8 @@ cases.map(kase =>
 )
 
 document.querySelector("input[name='displayCaseNumber']")
+document.querySelector("input[title='Search']")
+
+document.querySelectorAll("#toggleCase > td > table > tbody > tr")
+
+Array.prototype.map.call(document.querySelectorAll("#toggleCase > td > table > tbody > tr"), x=>x).map(row => Array.prototype.map.call(row.children, x=>x).map(cell => cell.innerText.trim()))

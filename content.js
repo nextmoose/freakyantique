71ffc4cd5fac52ae5a1340b5fb0a1db70b003cc2
Emory Merryman <emory.merryman@gmail.com>
2018-04-16 01:10:24 +0000

    better error message

--- content.js
+++ content.js
@@ -3,8 +3,14 @@ Date.prototype.format = function() { return ("0" + (this.getMonth() + 1)).slice(
 
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject("missing element"))(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
-const success = success => console.log({ status: true, success });
-const failure = failure => console.log({ status: false, failure });
+const success = success => {
+    console.log({ status: true, success });
+    return success;
+};
+const failure = failure => {
+    console.log({ status: false, failure });
+    return failure;
+};
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => console.log({ request, sender, sendResponse }));
 
@@ -22,11 +28,10 @@ querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > tab
             result: element.children[6].innerText
         }))
     )).then(chrome.runtime.sendMessage)
-    .then(
-        querySelector("input[value='Next']").then(element => element.click()),
-        failure => console.log("its over: "+failure)
+    .then(querySelector("input[value='Next']").then(element => element.click()))
         //        failure => querySelector("#txthearingdate").then(hearingDate => hearingDate.value="yesterday")
         //            .then(hearingDate => hearingDate.value = new Date(hearingDate.value).yesterday().format()
         //                .then(() => (new Date(hearingDate.value.getTime()>new Date("01/01/2018").getTime())) && querySelector("input[value='Search']").then(search => search.click()))
         //            )
-    ).then(success, failure);
+    .then(success, failure => querySelector("#txthearingdate").then(hearingDate => hearingDate.value="yesterday"))
+    .then(success, failure)

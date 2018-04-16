9a575a5cc78a7afefa979ce1b5c9f6cd8ca041a0
Emory Merryman <emory.merryman@gmail.com>
2018-04-16 00:01:55 +0000

    start it

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
-Date.prototype.yesterday = function(){return new Date(this.getTime()-24*60*60*1000)};
-Date.prototype.format = function(){return ("0"+(this.getMonth()+1)).slice(-2)+"/"+("0"+this.getDate()).slice(-2)+"/"+this.getFullYear()};
+Date.prototype.yesterday = function() { return new Date(this.getTime() - 24 * 60 * 60 * 1000) };
+Date.prototype.format = function() { return ("0" + (this.getMonth() + 1)).slice(-2) + "/" + ("0" + this.getDate()).slice(-2) + "/" + this.getFullYear() };
 
 const querySelector = selector => new Promise((resolve, reject) => (element => element ? resolve(element) : reject())(document.querySelector(selector)));
 const querySelectorAll = selector => new Promise((resolve, reject) => resolve(Array.prototype.map.call(document.querySelectorAll(selector), x => x)));
@@ -10,7 +10,7 @@ Promise.all([
         querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr"),
         querySelector("txthearingdate")
     ])
-    .then(array => ({rows: array[0], hearingDate: array[1]}))
+    .then(array => ({ rows: array[0], hearingDate: array[1] }))
     .then(object => object.rows
         .filter((element, index) => index > 0)
         .map(element => ({
@@ -25,4 +25,5 @@ Promise.all([
     ).then(chrome.runtime.sendMessage)
     .then(
         querySelector("input[value='Next']").then(element => element.click()),
-        failure => querySelector("txthearingdate").then(hearingDate => hearingDate.value=new Date(hearingDate.value).yesterday().format()))
+        failure => querySelector("txthearingdate").then(hearingDate => hearingDate.value = new Date(hearingDate.value).yesterday().format())
+    ).then(console.log, console.log);

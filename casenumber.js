5c75c3f114314f4b1218b9d685deb9cad9552f44
Emory Merryman <emory.merryman@gmail.com>
2018-04-17 12:30:34 +0000

    figured something out

--- casenumber.js
+++ casenumber.js
@@ -15,3 +15,21 @@ Array.prototype.map.call(document.querySelectorAll("#toggleCase > td > table > t
 
 
 [{a:1}, {b:2}, {c:3}].reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), [])
+
+
+[].concat.apply([],Array.prototype.map.call(document.querySelectorAll("#toggleCase > td > table > tbody > tr"), x=>x).map(row => Array.prototype.map.call(row.children, x=>x).map(cell => cell.innerText.trim()).map((cell, index, array)=> ({parity: index%2, key: array[Math.floor(index/2)*2], value: cell})).filter(element => element.parity===1)))
+    .reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), [])
+
+
+[].concat.apply([],Array.prototype.map.call(document.querySelectorAll("#toggleCase > td > table > tbody > tr"), x => x)
+    .map(row => Array.prototype.map.call(row.children, x => x)
+        .map(element => element.innerText.trim())
+        .map((element, index, array) => ({
+            parity: index%2,
+            key: array[index - (index%2)],
+            value: element
+        }))
+        .filter(element => element.parity===1)
+        .map(element => ({[element.key]: element.value}))
+    )
+)

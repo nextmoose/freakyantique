cd1cf3ecbf45c8da8e27f6cc393f2a801b6d9e5a
Emory Merryman <emory.merryman@gmail.com>
2018-04-17 12:05:11 +0000

    try accumulator

--- casenumber.js
+++ casenumber.js
@@ -12,3 +12,6 @@ document.querySelectorAll("#toggleCase > td > table > tbody > tr")
 Array.prototype.map.call(document.querySelectorAll("#toggleCase > td > table > tbody > tr"), x=>x).map(row => Array.prototype.map.call(row.children, x=>x).map(cell => cell.innerText.trim()).map((cell, index, array)=> ({parity: index%2, key: array[Math.floor(index/2)*2], value: cell})).filter(element => element.parity===1))
 
 Array.prototype.map.call(document.querySelectorAll("#toggleCase > td > table > tbody > tr"), x=>x).map(row => Array.prototype.map.call(row.children, x=>x).map(cell => cell.innerText.trim()))
+
+
+[{a:1}, {b:2}, {c:3}].reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), [])

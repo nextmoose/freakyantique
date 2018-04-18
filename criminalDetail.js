e258f0787c5eaacbaf0ec1ea475ca36136940af3
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 00:27:52 +0000

    try this

--- criminalDetail.js
+++ criminalDetail.js
@@ -1,16 +1,18 @@
-querySelectorAll("#toggleCase > td > table > tbody > tr")
-    .then(elements => [].concat.apply([], Array.prototype.map.call(elements, x => x)
-        .map(row => Array.prototype.map.call(row.children, x => x)
-            .map(element => element.innerText.trim())
-            .map((element, index, array) => ({
-                parity: index % 2,
-                key: array[index - (index % 2)],
-                value: element
-            }))
-            .filter(element => element.parity === 1)
-            .map(element => ({
-                [element.key]: element.value
-            }))
-        )
-    ).reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), []))
-    .then(chrome.runtime.sendMessage).then(() => window.close(), 10000)
+Promise.all(["toggleCase"].map(id => querySelectorAll("#"+id+" > td > table > tbody > tr")
+        .then(elements => [].concat.apply([], Array.prototype.map.call(elements, x => x)
+            .map(row => Array.prototype.map.call(row.children, x => x)
+                .map(element => element.innerText.trim())
+                .map((element, index, array) => ({
+                    parity: index % 2,
+                    key: array[index - (index % 2)].slice(0, -2),
+                    value: element
+                }))
+                .filter(element => element.parity === 1)
+                .map(element => ({
+                    [element.key]: element.value
+                }))
+            )
+        ).reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), []))
+        .then(object => ({[id]: object}))
+    ))
+    .then(chrome.runtime.sendMessage).then(() => window.close())

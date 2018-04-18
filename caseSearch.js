b282b08658b53ef1d006fe6c329af6b10ccbae87
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 02:56:45 +0000

    100 ms is too fast

--- caseSearch.js
+++ caseSearch.js
@@ -3,7 +3,7 @@ querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > tab
         .map(element => element.children[1].children[0])
         .map(element => (element.setAttribute('target', '_blank'), element))
         .map((element, index) => (console.log(index), element))
-        .map((element, index) => sleep(100 * index, element).then(() => element.click()))
+        .map((element, index) => sleep(500 * index, element).then(() => element.click()))
     ).then(
         querySelector("input[value='Next']").then(element => (element && !element.disabled) ? element.click() : querySelector("#txthearingdate").then(hearingDate => hearingDate.value = hearingDate.value==="" ? "08/01/2020" : new Date(hearingDate.value).yesterday().format()).then(() => querySelector("input[value='Search']")).then(element => element.click()))
     ).then(success, failure)

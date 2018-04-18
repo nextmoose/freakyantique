df26467d3330d7244c1ac83b448f8cbab97e7bf5
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 01:06:52 +0000

    wtf

--- criminalDetail.js
+++ criminalDetail.js
@@ -28,7 +28,21 @@ Promise.all([
                     [element.key]: element.value
                 }))
             )
+        ).reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), [])),
+        querySelectorAll("#toggleDisposition > td > table > tbody > tr")
+        .then(elements => [].concat.apply([], Array.prototype.map.call(elements, x => x)
+            .map(row => Array.prototype.map.call(row.children, x => x)
+                .map(element => element.innerText.trim())
+                .map((element, index, array) => ({
+                    parity: index % 2,
+                    key: array[index - (index % 2)].slice(0, -2),
+                    value: element
+                }))
+                .filter(element => element.parity === 1)
+                .map(element => ({
+                    [element.key]: element.value
+                }))
+            )
         ).reduce((accumulator, currentValue) => Object.assign(currentValue, accumulator), []))
-
-    ]).then(array => ({ "caseInformation": array[0], "charge": array[1] }))
+    ]).then(array => ({ "caseInformation": array[0], "charge": array[1], "disposition": array[2] }))
     .then(chrome.runtime.sendMessage).then(() => window.close())

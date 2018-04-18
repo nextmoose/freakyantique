17b604f2de8252cd27f9e49316a788617da2fc66
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 12:09:58 +0000

    wait 5 seconds

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => [
-    () => setTimeout(() => (console.log(request), sendResponse), 1000),
-    () => setTimeout(() => (console.log(request), sendResponse), 1000),
-    () => setTimeout(() => (console.log(request), sendResponse), 1000),
-    () => setTimeout(() => (console.log(request), sendResponse), 1000)
-][request.type]())
+    () => setTimeout(() => (console.log(request), sendResponse), 5000),
+    () => setTimeout(() => (console.log(request), sendResponse), 5000),
+    () => setTimeout(() => (console.log(request), sendResponse), 5000),
+    () => setTimeout(() => (console.log(request), sendResponse), 5000)
+][request.type](request, sender))

f2c4c173c3d0bfa3b27fc5420eff3f9182ed4a9c
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 00:28:33 +0000

    dow not filter to one element

--- caseSearch.js
+++ caseSearch.js
@@ -1,6 +1,5 @@
 querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
     .then(rows => rows.filter((element, index) => index > 0)
-        .filter((element, index) => index === 1)
         .map(element => element.children[1].children[0])
         .map(element => [element.setAttribute('target', '_blank'), element.click()])
         // ).then(

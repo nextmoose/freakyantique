dcb0438e2992c8cbd84b6f78214138f1aea90f51
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 12:13:51 +0000

    wtf

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
+
+const requests = [];
+
+const handler = () => setTimeout(() => (requests.push(request), console.log(request), sendResponse), 5000)
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => [
-    () => setTimeout(() => (console.log(request), sendResponse), 5000),
-    () => setTimeout(() => (console.log(request), sendResponse), 5000),
-    () => setTimeout(() => (console.log(request), sendResponse), 5000),
-    () => setTimeout(() => (console.log(request), sendResponse), 5000)
+    handler, handler, handler, handler, handler, handler
 ][request.type](request, sender))

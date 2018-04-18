f803df645f4b454a33370b7a7d267a3a776deb70
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 02:04:34 +0000

    wait for confirmation before closing

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 const cases=[];
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => (cases.push(request)), sendResponse());
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => (cases.push(request), sendResponse()));
 // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => console.log({request, sender, sendResponse, cases}));

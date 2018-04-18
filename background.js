5a6895c31022c9877e5a29b6119b36340ecb170e
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 12:08:06 +0000

    wtf

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => [
-    () => setTimeout(() => (console.log(request.value), sendResponse), 1000),
-    () => setTimeout(() => (console.log(request.value), sendResponse), 1000),
-    () => setTimeout(() => (console.log(request.value), sendResponse), 1000),
-    () => setTimeout(() => (console.log(request.value), sendResponse), 1000)
+    () => setTimeout(() => (console.log(request), sendResponse), 1000),
+    () => setTimeout(() => (console.log(request), sendResponse), 1000),
+    () => setTimeout(() => (console.log(request), sendResponse), 1000),
+    () => setTimeout(() => (console.log(request), sendResponse), 1000)
 ][request.type]())

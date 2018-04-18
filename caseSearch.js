630bd830671d69bff5b144fe0bda714da941db8a
Emory Merryman <emory.merryman@gmail.com>
2018-04-18 01:46:02 +0000

    log index

--- caseSearch.js
+++ caseSearch.js
@@ -1,7 +1,8 @@
-querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
+document.querySelectorAll("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr")
     .then(rows => rows.filter((element, index) => index > 0)
         .map(element => element.children[1].children[0])
         .map(element => (element.setAttribute('target', '_blank'), element))
+        .map((element, index) => (console.log(index), element))
         .map((element,index) => sleep(2000*index, element).then(() => element.click()))
         // ).then(
         //     querySelector("input[value='Next']").then(

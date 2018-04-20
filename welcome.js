c8d80d12be85625b3c7f22b8f47ca744ce659a2f
Emory Merryman <emory.merryman@gmail.com>
2018-04-20 16:57:48 +0000

    fix mispelling

--- welcome.js
+++ welcome.js
@@ -4,5 +4,8 @@ querySelector("#txtcourts1")
         querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a"),
         createEvent("MouseEvents")
     ]))
-    .then(array => (array[1].initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null), array[0].dispatchEvent(array[0])))
+    .then(array => (
+        console.log(array[0]),
+        array[1].initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null),
+        array[0].dispatchEvent(array[0])))
     .then(success, failure)

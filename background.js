9d9a46ce801005a6600b00f84861a655ebbb15c2
Emory Merryman <emory.merryman@gmail.com>
2018-04-20 18:56:04 +0000

    try one page at a time

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 
+const pages = [] ;
+
+const page = (request, sender, sendResponse) => pages.length===0 ? (pages.push(request), sendResponse(true)) : sendResponse(false);
+
 const requests = [];
 
 const handler = (request, handler) => setTimeout(() => (requests.push(request), console.log(request), () => sendResponse(requests.length)), 5000)
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => [
-    handler, handler, handler, handler, handler, handler
-][request.type](request, sender))
+    page, handler, handler, handler, handler, handler
+][request.type](request, sender, sendResponse))

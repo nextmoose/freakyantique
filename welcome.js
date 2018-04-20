3ae1c8063f24c5b30fbd3d1de07244d00ae19e78
Emory Merryman <emory.merryman@gmail.com>
2018-04-20 16:43:37 +0000

    try to dispatch

--- welcome.js
+++ welcome.js
@@ -1,4 +1,8 @@
 querySelector("#txtcourts1")
-    .then(element => element.value="Arlington General District Court")
-    .then(querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a"))
-    .then(element => element.click())
+    .then(element => element.value = "Arlington General District Court")
+    .then(Promise.all([
+        querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a"),
+        createEvent("MouseEvents")
+    ]))
+    .then(array => (array[1].initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null), array[0].dispatchEvent(array[0])))
+    .then(success, failure)

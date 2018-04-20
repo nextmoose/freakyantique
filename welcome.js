7dc8a6a1e5fdf1e97bdd7dcf6ade15c91a9b90ea
Emory Merryman <emory.merryman@gmail.com>
2018-04-20 13:27:11 +0000

    add pound sign

--- welcome.js
+++ welcome.js
@@ -1,4 +1,4 @@
-querySelector("txtcourts1")
+querySelector("#txtcourts1")
     .then(element => element.value="Arlington General District Court")
     .then(querySelector("body > table:nth-child(1) > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(2) > td.navigationback > div > table > tbody > tr > td > div.navigationback > div.trafficcriminaltab > div > ul > li:nth-child(3) > span > a"))
     .then(element => element.click())
